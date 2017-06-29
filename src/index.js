58c3d9419af7b17e8eb892cd0052a037d884f414
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 21:09:16 +0300

    Lesson 4_8 complete

--- src/index.js
+++ src/index.js
@@ -1,41 +1,35 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-import PropTypes from 'prop-types';
 
 class App extends Component {
 
-    static propTypes = {
-        btnText: PropTypes.string.isRequired,
-        mainTitle: PropTypes.string.isRequired,
-        paragraph: PropTypes.string.isRequired,
-        arrayApp: PropTypes.array.isRequired
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: 'test'
+        };
     }
 
-    static defaultProps = {
-        btnText: 'Simply click me!'
-    }
-
-    consoleLog() {
-        console.log('Boom');
+    textChange(event) {
+        const text = event.target.value;
+        this.setState({
+            text: text
+        });
+        console.log('textChange', text);
     }
 
     render() {
-        console.log('Props', this.props);
         return (
             <div style={ { backgroundColor: 'aliceblue' } }>
-                <h1 className="app__main-title">{ this.props.mainTitle }</h1>
-                <p className="app__paragraph">{ this.props.paragraph }</p>
-                <button className="app__btn" onClick={ this.consoleLog }>{ this.props.btnText }</button>
+                <h1 className="app__main-title">What is State...</h1>
+                <input type="text" value={ this.state.text } onChange={ this.textChange.bind(this) } />
             </div>
         );
     }
 }
 
 ReactDom.render(
-    <App btnText="Click and look in console"
-         mainTitle="This is App with React"
-         paragraph="Yes, people, yes, it`s true, it`s app with react"
-         arrayApp={ [1, 2, 3] }
-    />,
+    <App />,
     document.getElementById('app')
 );
