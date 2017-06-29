5046a246ff99a33274f519c251e289639cc7c951
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 19:21:41 +0300

    Lesson 4_5 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hi');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="app__main-title">This is App with React</h1>
+                <p className="app__paragraph">Yes, people, yes, it`s true, it`s app with react</p>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('app')
+);
