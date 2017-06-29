c1a7ff52c2c612b25114c31d6f232d28ef4455d1
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 20:21:44 +0300

    Lesson 4_7 complete

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
 
-class App extends React.Component {
+class App extends Component {
+
+    static propTypes = {
+        btnText: PropTypes.string.isRequired,
+        mainTitle: PropTypes.string.isRequired,
+        paragraph: PropTypes.string.isRequired,
+        arrayApp: PropTypes.array.isRequired
+    }
+
+    static defaultProps = {
+        btnText: 'Simply click me!'
+    }
 
     consoleLog() {
         console.log('Boom');
     }
 
     render() {
+        console.log('Props', this.props);
         return (
             <div style={ { backgroundColor: 'aliceblue' } }>
-                <h1 className="app__main-title">This is App with React</h1>
-                <p className="app__paragraph">Yes, people, yes, it`s true, it`s app with react</p>
-                <button className="app__btn" onClick={ this.consoleLog }>{ this.props.children }</button>
+                <h1 className="app__main-title">{ this.props.mainTitle }</h1>
+                <p className="app__paragraph">{ this.props.paragraph }</p>
+                <button className="app__btn" onClick={ this.consoleLog }>{ this.props.btnText }</button>
             </div>
         );
     }
 }
 
 ReactDom.render(
-    <App>Click and look in console</App>,
+    <App btnText="Click and look in console"
+         mainTitle="This is App with React"
+         paragraph="Yes, people, yes, it`s true, it`s app with react"
+         arrayApp={ [1, 2, 3] }
+    />,
     document.getElementById('app')
 );
