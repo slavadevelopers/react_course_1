07c7e0756741aba19cd2ea1f87b79557e07b6c5c
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 19:44:04 +0300

    Lesson 4_6 complete

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
+
+    consoleLog() {
+        console.log('Boom');
+    }
+
     render() {
         return (
-            <div>
+            <div style={ { backgroundColor: 'aliceblue' } }>
                 <h1 className="app__main-title">This is App with React</h1>
                 <p className="app__paragraph">Yes, people, yes, it`s true, it`s app with react</p>
+                <button className="app__btn" onClick={ this.consoleLog }>{ this.props.children }</button>
             </div>
         );
     }
 }
 
 ReactDom.render(
-    <App />,
+    <App>Click and look in console</App>,
     document.getElementById('app')
 );
