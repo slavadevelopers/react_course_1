1eefff863f7fe6bd807a1f3d0678b4ca1b55c0ba
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 21:46:16 +0300

    Lesson 4_10 complete

--- src/index.js
+++ src/index.js
@@ -6,13 +6,26 @@ class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            timer: 0
+        }
+    }
+
+    componentWillMount() {
+        setInterval(() => {
+            this.setState({
+                timer: this.state.timer + 1
+            })
+        }, 500)
     }
 
     render() {
         return (
             <div style={ { backgroundColor: 'aliceblue' } }>
-                <h1 className="app__main-title">What is State...</h1>
-                <New textContent="It`s description new component"/>
+                <h1 className="app__main-title">What is Lifecycle...</h1>
+                <p className="app__paragraph">Look in console, wait 5 sec.</p>
+                <p>{ this.state.timer }</p>
+                { this.state.timer <= 10 ? <New textContent="It`s description new component"/> : null }
             </div>
         );
     }
--- src/new.js
+++ src/new.js
@@ -4,9 +4,23 @@ class New extends Component {
 
     constructor(props) {
         super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+
+    componentDidMount() {
+        console.log('componenetDidMount');
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
 
     render() {
+        console.log('render');
         return (
             <div style={ { backgroundColor: 'beige' } }>
                 <h2 className="app__title">Title of new component</h2>
