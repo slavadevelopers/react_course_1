f65b94e9a7aa726729c6c0fa551e7f215d7b95bf
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-06-29 23:52:56 +0300

    Lesson 4_12 complete

--- src/index.js
+++ src/index.js
@@ -1,50 +1,32 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router';
 import ReactDom from 'react-dom';
 import New from './new';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const newHistory = createBrowserHistory();
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            array: [
-                {
-                    id: 1,
-                    text: 'It`s item number 1'
-                },
-                {
-                    id: 2,
-                    text: 'It`s item number 2'
-                },
-                {
-                    id: 3,
-                    text: 'It`s item number 3'
-                },
-                {
-                    id: 4,
-                    text: 'It`s item number 4'
-                },
-                {
-                    id: 5,
-                    text: 'It`s item number 5'
-                }
-            ]
-        }
     }
-
-
 
     render() {
         return (
             <div style={ { backgroundColor: 'aliceblue' } }>
-                <h1 className="app__main-title">What is it...</h1>
-                <New array={ this.state.array } />
+                <h1 className="app__main-title">App component</h1>
             </div>
         );
     }
 }
 
 ReactDom.render(
-    <App />,
+    <Router history={newHistory}>
+        <div>
+            <Route path={'/'} component={ App } />
+            <Route path={'/new'} component={ New } />
+        </div>
+    </Router>,
     document.getElementById('app')
 );
--- src/new.js
+++ src/new.js
@@ -4,23 +4,12 @@ class New extends Component {
 
     constructor(props) {
         super(props);
-        console.log('constructor');
     }
-
-    renderItem(item) {
-       return(
-           <li key={ item.id }>{ item.text }</li>
-       );
-    }
-
 
     render() {
         return (
             <div style={ { backgroundColor: 'beige' } }>
-                <h2 className="app__title">Title of new component</h2>
-                <ul>
-                    {this.props.array.map(this.renderItem.bind(this))}
-                </ul>
+                <h2 className="app__title">New component</h2>
             </div>
         );
     }
